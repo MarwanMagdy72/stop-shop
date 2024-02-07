c61ec1516bdcadc8411254775eee6daec240e99b
MarwanMagdy72 <115204176+MarwanMagdy72@users.noreply.github.com>
2024-02-07 03:28:19 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/js/bootstrap.bundle";
+import "font-awesome/css/font-awesome.min.css";
+import "./index.css";
+import App from "./App";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
